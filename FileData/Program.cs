07eaf1642fa8d210bfbf94ad3160e94ce3e62b4e
Hamid Iqbal <hamid.iqbal@avanade.com>
2018-03-23 14:46:54 +0000

    HI-Technical Test

--- FileData/Program.cs
+++ FileData/Program.cs
@@ -9,6 +9,21 @@ namespace FileData
     {
         public static void Main(string[] args)
         {
+            FileDetails obj = new FileDetails();
+            if (args[0] == "-v" || args[0] == "--v" || args[0] == "/v" || args[0] == "--version")
+            {
+                System.Console.WriteLine("File version is--" + obj.Version(args[0]));
+
+            }
+            else if (args[0] == "-s" || args[0] == "--s" || args[0] == "/s" || args[0] == "--size")
+
+            {
+                System.Console.WriteLine("File Size is--" + obj.Size(args[0]));
+
+            }
+            else System.Console.WriteLine("Invalid input please check");
+            
+
         }
     }
 }
